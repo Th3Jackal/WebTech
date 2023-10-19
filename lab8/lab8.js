9dd20efa727b9c2bdcfb79873d007f7afa1b9538
Daniel Albasini <99764455+Th3Jackal@users.noreply.github.com>
2023-10-19 13:50:00 -0500

    Add files via upload

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -1,14 +1,17 @@
 
 function getDataFromForm(fname, lname) {
-  alert("it worked!");
+  //alert("it worked!");
   runAjax(fname, lname);
 }
 
 function runAjax(fname, lname) {
   const xhttp = new XMLHttpRequest();
-  xhttp.onload = function() {
-    document.getElementById("responseString").innerHTML = this.responseText;
-  }
   xhttp.open("GET", "ajax.php?fname="+fname+"&lname="+lname, true);
+  xhttp.onload = function() {
+    if(xhttp.readyState == 4 && xhttp.status == 200) {
+      document.getElementById("responseString").innerHTML = this.responseText;
+    }
+  }
+  //xhttp.open("GET", "ajax.php?fname="+fname+"&lname="+lname, true);
   xhttp.send();
 }
