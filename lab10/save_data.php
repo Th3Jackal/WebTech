a81cc6d3a1a58dee89c230f4b5a51eef8a98d764
Daniel Albasini <99764455+Th3Jackal@users.noreply.github.com>
2023-11-05 15:46:49 -0600

    Add files via upload

--- lab10/save_data.php
+++ lab10/save_data.php
@@ -4,7 +4,9 @@ $file = fopen("data.txt","a+");
 $fnamestr = $_GET['fname'];
 $lnamestr = $_GET['lname'];
 echo fwrite($file,$fnamestr);
+echo fwrite($file,$_GET['fname']);
 echo fwrite($file,$lnamestr);
+echo fwrite($file,$_GET['lname']);
 fclose($file);
 
 ?> 
