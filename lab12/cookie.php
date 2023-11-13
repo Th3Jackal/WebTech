97af4890e038fd40b0a276023ed6fcd54dcd4cec
Daniel Albasini <99764455+Th3Jackal@users.noreply.github.com>
2023-11-12 20:43:31 -0600

    Add files via upload

---
+++ lab12/cookie.php
@@ -0,0 +1,12 @@
+<?php
+    $cname = "Test";
+    $cvalue = "yes";
+    if(isset($_COOKIE[$cname]))
+    {
+        echo "<p>Cookie '" . $cname . "' is set with value of " . $cvalue . ".</p>"; 
+    }
+    $else
+    {
+        setcookie("Test", $cvalue, time()+90, "/");
+    }
+?>
